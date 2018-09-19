e216f5a9183ef5e438552db359bbb9680ce95e17
Mukam <christa.mukam@sap.com>
2018-09-19 17:28:50 +0200

    changes

--- intro_android_demo-master/app/src/main/java/codepath/apps/demointroandroid/ActionBarMenuActivity.java
+++ intro_android_demo-master/app/src/main/java/codepath/apps/demointroandroid/ActionBarMenuActivity.java
@@ -7,8 +7,12 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import com.pusher.pushnotifications.PushNotifications;
+
+
 
 public class ActionBarMenuActivity extends Activity {
+
 
 	@SuppressLint("NewApi")
 	@Override
@@ -16,12 +20,16 @@ public class ActionBarMenuActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_action_bar_menu);
 		getActionBar().setTitle("Click an Icon");
+		//PushNotifications.start(getApplicationContext(), "e68b3ab5-e4a5-4a8b-a2c7-ca3c00c75d98");
+		//PushNotifications.subscribe("hello");
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_action_bar_menu, menu);
+		//PushNotifications.start(getApplicationContext(), "e68b3ab5-e4a5-4a8b-a2c7-ca3c00c75d98");
+		//PushNotifications.subscribe("hello");
 		return true;
 	}
 	
